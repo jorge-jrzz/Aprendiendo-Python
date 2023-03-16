01ed24e43a631ca6c02ba9250015e4f7202e1a8d
jorge Juarez <jorgeang33@gmail.com>
2023-03-16 13:13:41 -0600

    Strings like chains

---
+++ Tipos_basicos/02-Strings.py
@@ -0,0 +1,17 @@
+nombre_curso = "Ultimate Python"
+descripcion_curso = """
+Ultimate Python, 
+Este curso contemplta todos los detalles 
+que necesitas para conseguir un trabajo 
+como programador de Python.
+"""
+print(nombre_curso, descripcion_curso)
+
+print(len(nombre_curso))  # Longitud de la cadena.
+print(nombre_curso[0])  # Caracteres en la posición 0.
+print(nombre_curso[0:8])  # Caracteres de la posición 0 a la 8.
+# Caracteres de la posición 9 hasta la ultima posición.
+print(nombre_curso[9:])
+# Caracteres de la posición 9 hasta la ultima posición.
+print(nombre_curso[:8])
+print(nombre_curso[:])  # Caracteres de la posición 0 hasta la ultima posición.
