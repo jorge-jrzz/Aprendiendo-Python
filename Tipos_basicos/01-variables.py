ce70df219f1abde106286ef9a89f80c17f018eea
jorge Juarez <jorgeang33@gmail.com>
2023-03-16 13:01:37 -0600

    Update file

---
+++ Tipos_basicos/01-variables.py
@@ -0,0 +1,11 @@
+# NOMBRES DE VARIABLES
+
+nombre_curso = "Ultimate Python"
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NoMbRe_CurSo = "Feliz"
+NombreCurso = "Chanchito"
+print(nombre_curso, nombre1, NOMBRE_CURSO)
+alumnos = 5000  # Integer
+puntaje = 9.9  # Float
+publicado = True  # Boolean
