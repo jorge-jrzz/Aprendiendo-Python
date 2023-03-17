d8c3e8244cf714d008e6f6ec4ecac2f92f13173a
Jorge Juarez <jorgeang33@gmail.com>
2023-03-17 11:09:12 -0600

    add  07-numeros-funciones.py

---
+++ Tipos_basicos/07-numeros-funciones.py
@@ -0,0 +1,29 @@
+import math
+
+# Redondeo al valor entero mas cercano
+print(round(1.3))
+print(round(1.7))
+print(round(1.5))
+
+# Obtiene el valor absoluto
+print(abs(77))
+print(abs(-54))
+
+# Metos del modulo Math
+
+# Reondea al numero entero mas cernao (siempre hacia arriba)
+print(math.ceil(1.1))
+print(math.ceil(1.9))
+
+# Reondea al numero entero mas cernao (siempre hacia abajo)
+print(math.floor(1.999999))
+print(math.floor(1.04))
+
+# Si es que el valor que nosotros le estamos pasando entre paréntesis corresponde a que no es un número.
+print(math.isnan(23))
+
+# Operacion de potencia
+print(math.pow(10, 3))
+
+# Operacion de raiz cuadrada
+print(math.sqrt(9))
