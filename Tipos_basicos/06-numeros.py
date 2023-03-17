4b4a8eccc1ba052092ef239407db96d9c2b388a4
Jorge Juarez <jorgeang33@gmail.com>
2023-03-17 10:50:12 -0600

    Update 06-numeros.py

---
+++ Tipos_basicos/06-numeros.py
@@ -0,0 +1,18 @@
+numero = 2  # Integer
+decimal = 1.2  # float
+imaginario = 2 + 2j
+
+# Operaciones
+print(1 + 3)    # Suma
+print(1 - 3)    # Resta
+print(1 * 3)    # Multiplicacion
+print(1 / 3)    # Division
+print(1 // 3)   # Division, pero solo obtiene el valor entero (elimina los decimales)
+print(1 % 3)    # Operacion modulo
+print(2 ** 3)   # Potencia (2^3)
+
+# numero = numero + 2
+numero += 2
+numero -= 2
+numero *= 2
+numero /= 2
