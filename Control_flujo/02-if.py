df98164d84abd65fc669bb41fc60ada20698de6c
Jorge Juarez <jorgeang33@gmail.com>
2023-03-18 17:40:46 -0600

    decision oerators

---
+++ Control_flujo/02-if.py
@@ -0,0 +1,11 @@
+age = 77
+if age > 65:
+    print("Pude ver la pelicula con un super descuento.")
+elif age > 54:
+    print("Pude ver la pelicula con descuento.")
+elif age > 17:
+    print("Puedes ver la pelicula.")
+else:
+    print("No puesde entrar")
+
+print("Listo")
---
+++ Control_flujo/03-if-ternario.py
@@ -0,0 +1,10 @@
+age = 19
+
+# if age > 17:
+#     mensaje = "Es mayor"
+# else:
+#     mensaje = "es menor"
+
+mensaje = "Es mayor" if age > 17 else "es menor"  # IF ternario
+
+print(mensaje)
