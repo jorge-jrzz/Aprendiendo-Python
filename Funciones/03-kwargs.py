26d14b099c30f7ea4d795a88b272440477a7095b
Jorge Juarez <jorgeang33@gmail.com>
2023-03-19 16:57:26 -0600

    add funtions

---
+++ Funciones/03-kwargs.py
@@ -0,0 +1,13 @@
+# Esta funcion imprime especificamento lo indicado
+
+# def get_product(**datos):
+#     print(datos["id"], datos["name"])
+
+def get_product(**datos):
+    print(datos)
+
+
+get_product(id="13",
+            name="iPhone",
+            desc="Esto es un iPhone")
+# Esto lo que imprime es uun diccionario
---
+++ Funciones/04-return.py
@@ -0,0 +1,7 @@
+def suma(a, b):
+    return a + b
+
+
+resultado = suma(2, 3)
+print(resultado)
+print(suma(3, 4))
