7b68bb28c882c759b9a1798aef0cd0d7ae613b8f
Jorge Juarez <jorgeang33@gmail.com>
2023-03-18 23:20:14 -0600

    do-while struct

--- Control_flujo/07-while.py
+++ Control_flujo/07-while.py
@@ -1,4 +1,20 @@
 numero = 1
-while numero < 100:
-    print(numero)
-    numero *= 2
+# while numero < 100:
+#     print(numero)
+#     numero *= 2
+
+
+# El siguinete code simula una terminar
+comando = ""
+while comando.lower() != "salir":
+    # Se ocupa el comando de lower para que el usuario pueda ingresar: $ SALIR, $ Salir o $ SaLiR
+    comando = input("$ ")
+    print(comando)
+
+''' 
+while True:
+    comando = input("$ ")
+    print(comando)
+    if comando.lower() == "salir":
+        break;
+'''
