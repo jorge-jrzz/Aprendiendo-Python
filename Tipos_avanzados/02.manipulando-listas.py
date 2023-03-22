acd021723ae56cb96748fd192fd30feaf24cd558
Jorge Juarez <jorgeang33@gmail.com>
2023-03-21 22:18:22 -0600

    manupulando listas

---
+++ Tipos_avanzados/02.manipulando-listas.py
@@ -0,0 +1,10 @@
+mascotas = ["Chelsea", "Toctil", "Caruso", "Lizzi"]
+print(mascotas[0])
+mascotas[0] = "Bicho"
+# print(mascotas[0:3])
+# print(mascotas[-1])
+# print(mascotas[::2])
+
+numeros = list(range(21))
+print(numeros[1::2])  # Numeros Impares
+print(numeros[::2])  # Numeros Pares
