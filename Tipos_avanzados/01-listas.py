67d7d95525ac64309db93eb244cfb1c4baa1cc71
Jorge Juarez <jorgeang33@gmail.com>
2023-03-21 21:57:34 -0600

    introduccion a las listas

---
+++ Tipos_avanzados/01-listas.py
@@ -0,0 +1,16 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["Chanchitos", "felices", "Jorge", "alumno"]
+boolenas = [True, False, True, True]
+
+matriz = [[0, 1], [1, 0]]
+
+ceros = [0] * 10  # Crea una lista con 10 ceros
+
+alfanumerico = numeros + letras
+
+# Crea una lista con numero que van en un rango del 1 al 10
+rango = list(range(1, 11))
+
+chars = list("Hello, World!")
+print(chars)
