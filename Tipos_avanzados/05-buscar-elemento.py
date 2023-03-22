2553ac6d523a287ac56ea3be1c05f8d80a91de8f
jorge Juarez <jorgeang33@gmail.com>
2023-03-22 11:58:46 -0600

    Busqueda en listas

---
+++ Tipos_avanzados/05-buscar-elemento.py
@@ -0,0 +1,7 @@
+mascotas = ["Pelusa", "Chelsea", "Pulga",
+            "Felipe", "Chelsea", "Chanchito feliz"]
+
+print(mascotas.count("Chelsea"))
+
+if "Chelsea" in mascotas:
+    print(mascotas.index("Chelsea"))
