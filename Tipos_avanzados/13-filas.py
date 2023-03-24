8173487ca6a9e9b95672fdd6c5364e32f9c6d611
Jorge Juarez <jorgeang33@gmail.com>
2023-03-23 19:16:21 -0600

    Estructura fila o cola

---
+++ Tipos_avanzados/13-filas.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+fila = deque([1, 2])
+fila.append(3)
+fila.append(4)
+fila.append(5)
+print(fila)
+
+fila.popleft()  # Quita el primer elemento
+print(fila)
+
+if not fila:
+    print("La fila esta vacia")
