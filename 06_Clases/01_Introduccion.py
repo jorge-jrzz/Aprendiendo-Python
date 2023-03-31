62ced5750ead900713a2a76267cf23cf0811fa81
jorge Juarez <jorgeang33@gmail.com>
2023-03-30 19:25:55 -0600

    Intoduccion

---
+++ 06_Clases/01_Introduccion.py
@@ -0,0 +1,11 @@
+mensaje = "Hola Mundo"
+print(type(mensaje))
+
+# Clase: Es el plano de coonstruccion
+# Objeto: Es una instancia de una clase
+
+# Clases: El plano de construcion de una casa
+# Objeto: La casa construida
+
+# Clase: Humano
+# Objeto: Jorge, Maria, Sebastian; Julio
