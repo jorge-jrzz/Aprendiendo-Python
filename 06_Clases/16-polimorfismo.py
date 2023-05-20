46c9e94d788908338a3c946ebdabed3bc0262683
Jorge Juarez <jorgeang33@gmail.com>
2023-05-20 12:59:07 -0600

    Conociendo el duck typing

--- 06_Clases/16-polimorfismo.py
+++ 06_Clases/16-polimorfismo.py
@@ -25,3 +25,25 @@ def guardar(entidades):
 usuario = Usuario()
 sesion = Sesion()
 guardar([sesion, usuario])
+
+
+# Duck Typing:
+
+# class Usuario():
+#     def guardar(self):
+#         print("Guardando en BBDD")
+
+
+# class Sesion():
+#     def guardar(self):
+#         print("Guardando en archivo")
+
+
+# def guardar(entidades):
+#     for entidad in entidades:
+#         entidad.guardar()
+
+
+# usuario = Usuario()
+# sesion = Sesion()
+# guardar([sesion, usuario])
