9de6b0e7a24741c21cea387caaf00940673e7981
Jorge Juarez <jorgeang33@gmail.com>
2023-06-01 17:10:21 -0600

    Descargando un archivo desde un servidor

---
+++ extra_APIs/07-download-file.py
@@ -0,0 +1,11 @@
+
+import requests
+
+URL = 'https://codigofacilito.com/images/cody'
+
+response = requests.get(URL, stream=True)
+
+if response.status_code == 200:
+    with open('images/cody.png', 'wb') as file:
+        for chunk in response.iter_content(1024):
+            file.write(chunk)
